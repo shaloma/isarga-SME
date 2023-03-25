21e6c239a34e101345b4cdef32395ca4b94314e0
shaloma <shaloma90@gmail.com>
2023-03-25 13:09:38 -0600

    Added sliders for carbohydrates, proteins, and lipids in the HTML file, along with labels to display the current percentage values. Added event listeners for the sliders in the JavaScript file to update the displayed values when the sliders are adjusted. Modified the calculate function in the JavaScript file to read the slider values and use them in the SME calculation.
    After making these changes, users should be able to adjust the nutrient distribution using the sliders, and the results will reflect their custom distribution choices.

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ document.getElementById("bmr-form").addEventListener("submit", function (event) 
   const gender = document.getElementById("gender").value;
   const activityLevel = parseFloat(document.getElementById("activity-level").value);
 
+  const carbsPercent = parseFloat(document.getElementById("carbs").value);
+  const proteinsPercent = parseFloat(document.getElementById("proteins").value);
+  const lipidsPercent = parseFloat(document.getElementById("lipids").value);
+
   let bmr;
   if (gender === "male") {
     bmr = 10 * weight + 6.25 * height - 5 * age + 5;
@@ -22,13 +26,26 @@ document.getElementById("bmr-form").addEventListener("submit", function (event) 
     <h2>Tu Gasto Energético Total Diario (TDEE) es: ${tdee.toFixed(2)} kcal/día</h2>
     <p>Con base en el SME, se recomienda consumir:</p>
     <ul>
-      <li>${(tdee * 0.55 / 4).toFixed(1)} equivalentes de cereales y tubérculos</li>
-      <li>${(tdee * 0.15 / 4).toFixed(1)} equivalentes de frutas</li>
-      <li>${(tdee * 0.10 / 4).toFixed(1)} equivalentes de leguminosas</li>
-      <li>${(tdee * 0.10 / 4).toFixed(1)} equivalentes de alimentos de origen animal</li>
-      <li>${(tdee * 0.10 / 9).toFixed(1)} equivalentes de grasas</li>
-    </ul>
-  `;
-
-  document.getElementById("result").innerHTML = result;
-});
+      <li>${((tdee * carbsPercent / 100) / 4).toFixed(1)} equivalentes de cereales y tubérculos</li>
+      <li>${((tdee * proteinsPercent / 100) / 4).toFixed(1)} equivalentes de frutas</li>
+      <li>${((tdee * lipidsPercent / 100) / 4).toFixed(1)} equivalentes de leguminosas</li>
+      <li>${((tdee * proteinsPercent / 100) / 4).toFixed(1)} equivalentes de alimentos de origen animal</li>
+      <li>${((tdee * lipidsPercent / 100) / 9).toFixed(1)} equivalentes de grasas</li>
+      </ul>
+      `;
+      
+      document.getElementById("result").innerHTML = result;
+      });
+      
+      // Add event listeners for sliders to update the displayed values
+      document.getElementById("carbs").addEventListener("input", function () {
+      document.getElementById("carbs-value").textContent = this.value;
+      });
+      
+      document.getElementById("proteins").addEventListener("input", function () {
+      document.getElementById("proteins-value").textContent = this.value;
+      });
+      
+      document.getElementById("lipids").addEventListener("input", function () {
+      document.getElementById("lipids-value").textContent = this.value;
+      });
