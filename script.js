536d714d53da1d467dd239e85902fcf0d84ac078
shaloma <shaloma90@gmail.com>
2023-03-24 22:54:02 -0600

    Added physical activity and thermic effect of food

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 document.getElementById("bmr-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-  
-    const age = parseInt(document.getElementById("age").value);
-    const height = parseInt(document.getElementById("height").value);
-    const weight = parseInt(document.getElementById("weight").value);
-    const gender = document.getElementById("gender").value;
-  
-    let bmr;
-    if (gender === "male") {
-      bmr = 10 * weight + 6.25 * height - 5 * age + 5;
-    } else {
-      bmr = 10 * weight + 6.25 * height - 5 * age - 161;
-    }
-  
-    const result = `
-      <h2>Tu Tasa Metabólica Basal (BMR) es: ${bmr.toFixed(2)} kcal/día</h2>
-      <p>Con base en el SME, se recomienda consumir:</p>
-      <ul>
-      <li>${(bmr * 0.55 / 4).toFixed(1)} equivalentes de cereales y tubérculos</li>
-      <li>${(bmr * 0.15 / 4).toFixed(1)} equivalentes de frutas</li>
-      <li>${(bmr * 0.10 / 4).toFixed(1)} equivalentes de leguminosas</li>
-      <li>${(bmr * 0.10 / 4).toFixed(1)} equivalentes de alimentos de origen animal</li>
-      <li>${(bmr * 0.10 / 9).toFixed(1)} equivalentes de grasas</li>
+  event.preventDefault();
+
+  const age = parseInt(document.getElementById("age").value);
+  const height = parseInt(document.getElementById("height").value);
+  const weight = parseInt(document.getElementById("weight").value);
+  const gender = document.getElementById("gender").value;
+  const activityLevel = parseFloat(document.getElementById("activity-level").value);
+
+  let bmr;
+  if (gender === "male") {
+    bmr = 10 * weight + 6.25 * height - 5 * age + 5;
+  } else {
+    bmr = 10 * weight + 6.25 * height - 5 * age - 161;
+  }
+
+  const tef = bmr * 0.1;
+  const tdee = bmr * activityLevel + tef;
+
+  const result = `
+    <h2>Tu Tasa Metabólica Basal (BMR) es: ${bmr.toFixed(2)} kcal/día</h2>
+    <h2>Tu Gasto Energético Total Diario (TDEE) es: ${tdee.toFixed(2)} kcal/día</h2>
+    <p>Con base en el SME, se recomienda consumir:</p>
+    <ul>
+      <li>${(tdee * 0.55 / 4).toFixed(1)} equivalentes de cereales y tubérculos</li>
+      <li>${(tdee * 0.15 / 4).toFixed(1)} equivalentes de frutas</li>
+      <li>${(tdee * 0.10 / 4).toFixed(1)} equivalentes de leguminosas</li>
+      <li>${(tdee * 0.10 / 4).toFixed(1)} equivalentes de alimentos de origen animal</li>
+      <li>${(tdee * 0.10 / 9).toFixed(1)} equivalentes de grasas</li>
     </ul>
   `;
 
