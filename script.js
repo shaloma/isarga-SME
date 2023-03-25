5ca401d79c2f40d99e46286edd9161fbe8be2d59
shaloma <shaloma90@gmail.com>
2023-03-25 13:30:49 -0600

    This updated code calculates the difference between the sum of the sliders and 100, then adjusts the other two sliders proportionally to compensate for this difference. This should provide a more intuitive user experience when adjusting the sliders.

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ document.getElementById("bmr-form").addEventListener("submit", function (event) 
   const gender = document.getElementById("gender").value;
   const activityLevel = parseFloat(document.getElementById("activity-level").value);
 
-  const carbsPercent = parseFloat(document.getElementById("carbs").value);
-  const proteinsPercent = parseFloat(document.getElementById("proteins").value);
-  const lipidsPercent = parseFloat(document.getElementById("lipids").value);
-
   let bmr;
   if (gender === "male") {
     bmr = 10 * weight + 6.25 * height - 5 * age + 5;
@@ -26,26 +22,56 @@ document.getElementById("bmr-form").addEventListener("submit", function (event) 
     <h2>Tu Gasto Energético Total Diario (TDEE) es: ${tdee.toFixed(2)} kcal/día</h2>
     <p>Con base en el SME, se recomienda consumir:</p>
     <ul>
-      <li>${((tdee * carbsPercent / 100) / 4).toFixed(1)} equivalentes de cereales y tubérculos</li>
-      <li>${((tdee * proteinsPercent / 100) / 4).toFixed(1)} equivalentes de frutas</li>
-      <li>${((tdee * lipidsPercent / 100) / 4).toFixed(1)} equivalentes de leguminosas</li>
-      <li>${((tdee * proteinsPercent / 100) / 4).toFixed(1)} equivalentes de alimentos de origen animal</li>
-      <li>${((tdee * lipidsPercent / 100) / 9).toFixed(1)} equivalentes de grasas</li>
-      </ul>
-      `;
-      
-      document.getElementById("result").innerHTML = result;
+      <li>${(tdee * 0.55 / 4).toFixed(1)} equivalentes de cereales y tubérculos</li>
+      <li>${(tdee * 0.15 / 4).toFixed(1)} equivalentes de frutas</li>
+      <li>${(tdee * 0.10 / 4).toFixed(1)} equivalentes de leguminosas</li>
+      <li>${(tdee * 0.10 / 4).toFixed(1)} equivalentes de alimentos de origen animal</li>
+      <li>${(tdee * 0.10 / 9).toFixed(1)} equivalentes de grasas</li>
+    </ul>
+  `;
+
+  document.getElementById("result").innerHTML = result;
+});
+
+// Function to update slider values and maintain 100% total
+function updateSlider(slider, valueElement) {
+  valueElement.textContent = slider.value;
+
+  const carbsSlider = document.getElementById("carbs");
+  const proteinsSlider = document.getElementById("proteins");
+  const lipidsSlider = document.getElementById("lipids");
+
+  const carbsValue = parseFloat(carbsSlider.value);
+  const proteinsValue = parseFloat(proteinsSlider.value);
+  const lipidsValue = parseFloat(lipidsSlider.value);
+
+  const total = carbsValue + proteinsValue + lipidsValue;
+  const diff = total - 100;
+
+  if (diff !== 0) {
+    const otherSliders = [
+      { id: "carbs", slider: carbsSlider, value: carbsValue },
+      { id: "proteins", slider: proteinsSlider, value: proteinsValue },
+      { id: "lipids", slider: lipidsSlider, value: lipidsValue },
+    ].filter(s => s.id !== slider.id);
+
+    const totalOtherSliders = otherSliders[0].value + otherSliders[1].value;
+    otherSliders.forEach(s => {
+      const adjustedValue = s.value - (s.value / totalOtherSliders) * diff;
+      s.slider.value = adjustedValue;
+      document.getElementById(s.id + "-value").textContent = adjustedValue.toFixed(0);
       });
+      }
+      }
       
-      // Add event listeners for sliders to update the displayed values
-      document.getElementById("carbs").addEventListener("input", function () {
-      document.getElementById("carbs-value").textContent = this.value;
-      });
-      
-      document.getElementById("proteins").addEventListener("input", function () {
-      document.getElementById("proteins-value").textContent = this.value;
-      });
-      
-      document.getElementById("lipids").addEventListener("input", function () {
-      document.getElementById("lipids-value").textContent = this.value;
-      });
+  document.getElementById("carbs").addEventListener("input", function () {
+  updateSlider(this, document.getElementById("carbs-value"));
+  });
+  
+  document.getElementById("proteins").addEventListener("input", function () {
+  updateSlider(this, document.getElementById("proteins-value"));
+  });
+  
+  document.getElementById("lipids").addEventListener("input", function () {
+  updateSlider(this, document.getElementById("lipids-value"));
+  });
